87269416d68bd303f28956ac6ace9d9ea7b7cc35
Michal Bursa <bursa@astro.cas.cz>
2016-03-23 16:28:36 +0100

    new routines for distribution

---
+++ lib/sim5distributions.h
@@ -0,0 +1,37 @@
+//************************************************************************
+//    sim5distributions.h
+//------------------------------------------------------------------------
+//    Date   : 10.12.2015
+//    Author : Michal Bursa
+//    e-mail : bursa@astro.cas.cz
+//------------------------------------------------------------------------
+//    (C) 2015 Michal Bursa
+//************************************************************************
+//************************************************************************
+
+
+
+#ifndef _SIM5DISTRIBUTIONS_H
+#define _SIM5DISTRIBUTIONS_H
+
+
+
+typedef struct sim5distrib {
+    double x_min;
+    double x_max;
+    double norm;
+    sim5interp pdf;                     // probability distribution function
+    sim5interp cdf;                     // cummulative distribution function
+    sim5interp icd;                     // inverse cummulative distribution function
+} sim5distrib;
+
+
+
+DEVICEFUNC void distrib_init(sim5distrib* d, double(*pdf)(double), double x_min, double x_max);
+DEVICEFUNC void distrib_done(sim5distrib* d);
+DEVICEFUNC INLINE double distrib_hit(sim5distrib* d);
+
+
+#endif
+
+
